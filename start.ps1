c505111cddf779b650bae3335cd581c5ee869b2b
scher470 <120848089+nxpinhum5326@users.noreply.github.com>
2023-12-15 21:34:00 +0300

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -21,11 +21,11 @@ if($php -ne ""){
 }
 
 if($file -eq ""){
-	if(Test-Path "PocketMine-MP.phar"){
-	    $file = "PocketMine-MP.phar"
+	if(Test-Path "BloodMine-MP.phar"){
+	    $file = "BloodMine-MP.phar"
 	}else{
-	    echo "PocketMine-MP.phar not found"
-	    echo "Downloads can be found at https://github.com/pmmp/PocketMine-MP/releases"
+	    echo "BloodMine-MP.phar not found"
+	    echo "Downloads can be found at https://github.com/nxpinhum5326/BloodMine-MP/releases"
 	    pause
 	    exit 1
 	}
